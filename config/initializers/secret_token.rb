9df891c6ec00ec74411b796dc09f5e21104630db
Amol D. Joshi <amoldjoshi@gmail.com>
2013-12-27 09:18:44 -0800

    Creating Framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fieux::Application.config.secret_token = '1db7d60d0cb247b1c001e44e5c5a47a4342e2a91eed4a15d73185be600a15f3613a994d708bb8051f9bde2cbb9049b072c793e778b27c244d7e9aa330327cb16'
