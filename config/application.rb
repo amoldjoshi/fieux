7c2d5e4280abb78f19691327a4c0e52143ed47f6
Amol D Joshi <amoldjoshi@gmail.com>
2014-09-09 02:58:52 -0700

    application.rb update

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Fieux
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    #Amol added to avoid heroku error.
+    config.assets.initialize_on_precompile = false
   end
 end
