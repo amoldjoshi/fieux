665de8b6d17e44692bd46966bf6b1911e3be26cc
Amol D. Joshi <amoldjoshi@gmail.com>
2014-09-10 20:43:43 -0700

    testing publickey

--- app/models/weekly_add.rb
+++ app/models/weekly_add.rb
@@ -1,3 +1,4 @@
 class WeeklyAdd < ActiveRecord::Base
   attr_accessible :WeeklyAddID, :WeeklyAddName, :WeeklyAddType, :IsWeeklyFavorite, :targeturl
+  #comment to test public key.  Amol 9 - 10.
 end
